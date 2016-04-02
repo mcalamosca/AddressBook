a550a40273915aeee5e8b7de0b1336a0491e469c
mcalamosca <mcalamosca@gmail.com>
2016-04-02 17:59:49 -0400

    first commit

---
+++ app/src/main/java/edu/pitt/is1073/addressbook/AddressList.java
@@ -0,0 +1,13 @@
+package edu.pitt.is1073.addressbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddressList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_list);
+    }
+}

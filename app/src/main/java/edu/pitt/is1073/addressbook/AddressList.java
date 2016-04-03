7a278c66cce694aeeeefa2e20980bb53629b668f
mcala80 <mcalamosca@gmail.com>
2016-04-03 15:00:22 -0400

    DbUtils created, methods still incomplete

--- app/src/main/java/edu/pitt/is1073/addressbook/AddressList.java
+++ app/src/main/java/edu/pitt/is1073/addressbook/AddressList.java
@@ -9,5 +9,7 @@ public class AddressList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address_list);
+
+
     }
 }
---
+++ app/src/main/java/edu/pitt/is1073/addressbook/DbUtils.java
@@ -0,0 +1,84 @@
+package edu.pitt.is1073.addressbook;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Created by Micah Calamosca on 4/3/2016.
+ */
+public class DbUtils extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "contactDB";
+    public static final String TABLE_NAME = "mycontacts";
+    public static final int DATABASE_VERSION = 1;
+    private SQLiteDatabase db;
+
+
+    public static final String KEY_ROW_ID = "_id";
+    public static final String KEY_LASTNAME = "last_name";
+    public static final String KEY_FIRSTNAME = "first_name";
+    public static final String KEY_ADDRESS = "address";
+    public static final String KEY_ADDRESS2 = "address2";
+    public static final String KEY_CITY = "city";
+    public static final String KEY_STATE = "state";
+    public static final String KEY_ZIP = "zip";
+    public static final String KEY_COUNTRY = "country";
+    public static final String KEY_PHONE = "phone";
+    public static final String KEY_EMAIL = "email";
+
+
+    public static final String PROJECTION[] = {
+            KEY_ROW_ID,
+            KEY_LASTNAME,
+            KEY_FIRSTNAME,
+            KEY_ADDRESS,
+            KEY_ADDRESS2,
+            KEY_CITY,
+            KEY_STATE,
+            KEY_ZIP,
+            KEY_COUNTRY,
+            KEY_PHONE,
+            KEY_EMAIL
+    };
+
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_NAME + "("+KEY_ROW_ID
+            +" int(11) NOT NULL AUTO_INCREMENT,"+
+            KEY_LASTNAME+"  varchar(100) not null,"+
+            KEY_FIRSTNAME+"  varchar(100) not null,"+
+            KEY_ADDRESS+"  varchar(100) not null,"+
+            KEY_ADDRESS2+"  varchar(100) not null,"+
+            KEY_CITY+"  varchar(100) not null,"+
+            KEY_STATE+"  varchar(100) not null,"+
+            KEY_ZIP+"  varchar(100) not null,"+
+            KEY_COUNTRY+"  varchar(100) not null,"+
+            KEY_PHONE+"  varchar(100),"+
+            KEY_EMAIL+"  varchar(255) not null,"+
+            "PRIMARY KEY (`_id`));";
+
+    public DbUtils(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
+
+    public void InsertRow(String tableName, ContentValues vals){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insert(tableName, null, vals);
+    }
+
+    public void UpdateRow(String tableName, ContentValues vals){
+
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //leave alone
+    }
+}
